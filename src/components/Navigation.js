70935c0ef9deff2ae7c08f70248422e879d74856
Abigail Horley <abigail.horley@bluescopesteel.com>
2024-12-02 11:45:16 +1100

    add icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Entypo';
 import CompanyList from './CompanyList';
 import AddPerson from './AddPerson';
 import PeopleList from './PeopleList';
@@ -11,10 +12,26 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={LandingPage} />
-      <Tab.Screen name="Employees" component={PeopleList} />
-      <Tab.Screen name="Add Employee" component={AddPerson}/>
-      <Tab.Screen name="Company Directory" component={CompanyList} />
+      <Tab.Screen name="Home" component={LandingPage} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={'#941a1d'} size={'20px'} />
+          ),
+        }}/>
+      <Tab.Screen name="Employees" component={PeopleList} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="users" color={'#941a1d'} size={'20px'} />
+          ),
+        }}/>
+      <Tab.Screen name="Add Employee" component={AddPerson} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="add-user" color={'#941a1d'} size={'20px'} />
+          ),
+        }}/>
+      <Tab.Screen name="Company Directory" component={CompanyList} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="text-document-inverted" color={'#941a1d'} size={'20px'} />
+          ),
+        }}/>
     </Tab.Navigator>
     
   );
